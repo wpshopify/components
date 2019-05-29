dd7a0bb5917d81e0d168adf457b2e806f995cd8c
Andrew Robbins <arobbins@simpleblend.net>
2019-05-28 22:19:22 -0500

    Added custom domain support

--- components/cart/checkout/index.jsx
+++ components/cart/checkout/index.jsx
@@ -12,11 +12,39 @@ function CartCheckout() {
    const [shopState] = useContext(ShopContext)
    const [cartState, cartDispatch] = useContext(CartContext)
 
-   function handleCheckout() {
-      checkout()
+   function hasManagedDomain(url) {
+      return url.includes('myshopify.com')
    }
 
-   async function checkout() {
+   function extractCheckoutURL(webUrl) {
+      return webUrl.split('myshopify.com')[1]
+   }
+
+   function checkoutUrlWithCustomDomain(webUrl) {
+      return shopState.info.primaryDomain.url + extractCheckoutURL(webUrl)
+   }
+
+   function managedDomainRedirect(checkout) {
+      window.open(checkout.webUrl, WP_Shopify.settings.checkoutButtonTarget)
+   }
+
+   function customDomainRedirect(checkout) {
+      window.open(checkoutUrlWithCustomDomain(checkout.webUrl), WP_Shopify.settings.checkoutButtonTarget)
+   }
+
+   function checkoutRedirect(checkout) {
+      if (!WP_Shopify.settings.enableCustomCheckoutDomain || !hasManagedDomain(checkout.webUrl)) {
+         return managedDomainRedirect(checkout)
+      }
+
+      customDomainRedirect(checkout)
+
+      if (WP_Shopify.settings.checkoutButtonTarget === '_blank') {
+         cartDispatch({ type: 'SET_IS_CHECKING_OUT', payload: false })
+      }
+   }
+
+   async function onCheckout() {
       cartDispatch({ type: 'SET_IS_CHECKING_OUT', payload: true })
 
       const [err, success] = await to(replaceLineItems(shopState.checkoutCache.lineItems))
@@ -34,10 +62,10 @@ function CartCheckout() {
             })
          )
 
-         window.open(resp.webUrl, WP_Shopify.settings.checkoutButtonTarget)
-      } else {
-         window.open(success.webUrl, WP_Shopify.settings.checkoutButtonTarget)
+         checkoutRedirect(resp)
       }
+
+      checkoutRedirect(success)
    }
 
    function createLineItems() {
@@ -51,7 +79,7 @@ function CartCheckout() {
 
    return (
       <>
-         <button className='wps-btn wps-btn-checkout' onClick={handleCheckout} data-wps-is-ready={shopState.isShopReady} disabled={cartState.isCheckingOut}>
+         <button className='wps-btn wps-btn-checkout' onClick={onCheckout} data-wps-is-ready={shopState.isShopReady} disabled={cartState.isCheckingOut}>
             {cartState.isCheckingOut ? <Loader isLoading={cartState.isCheckingOut} /> : cartState.checkoutText}
          </button>
       </>
